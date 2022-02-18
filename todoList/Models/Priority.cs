65ce321f465f8d3e21d0c229260e49c599e2c06c
Piotr Kłopotek Główczewski <pkglowczewski@gmail.com>
2022-02-18 11:38:48 +0100

    Create Priority model

---
+++ todoList/Models/Priority.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace todoList.Models
+{
+    public class Priority
+    {
+        [Key]
+        public int PriorityId { get; set; }
+        [Display(Name = "Priorytet")]
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+    }
+}
