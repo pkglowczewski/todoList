d223420c3eb17f28848a3377c44271742a7a0dc4
Piotr Kłopotek Główczewski <pkglowczewski@gmail.com>
2022-02-18 11:45:14 +0100

    Create ApplicationUser model

---
+++ todoList/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace todoList.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
