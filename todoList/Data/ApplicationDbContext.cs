9b3a9fb10d5c617ad486d6475820032ab2144492
Piotr Kłopotek Główczewski <pkglowczewski@gmail.com>
2022-02-18 11:58:42 +0100

    Update ApplicationDbContext

--- todoList/Data/ApplicationDbContext.cs
+++ todoList/Data/ApplicationDbContext.cs
@@ -1,13 +1,32 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using todoList.Models;
 
 namespace todoList.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<Models.Task> Tasks { get; set; }
+        public DbSet<Priority> Priorities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Models.Task>()
+                .HasOne<ApplicationUser>(u => u.User)
+                .WithMany(u => u.Tasks)
+                .HasForeignKey(c => c.UserId);
+
+            modelBuilder.Entity<Priority>().HasData(new Priority { PriorityId = 1, Name = "Bardzo wysoki"},
+                new Priority { PriorityId = 2, Name = "Wysoki"},
+                new Priority { PriorityId = 3, Name = "Średni"},
+                new Priority { PriorityId = 4, Name = "Niski"});
         }
     }
 }
