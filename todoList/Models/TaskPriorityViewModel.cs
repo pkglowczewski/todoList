47b3581530a65db51e8cdad660349ef2400b74df
Piotr Kłopotek Główczewski <pkglowczewski@gmail.com>
2022-02-18 12:30:16 +0100

    Create TaskPriorityViewModel

---
+++ todoList/Models/TaskPriorityViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace todoList.Models
+{
+    public class TaskPriorityViewModel
+    {
+        public Priority Priority { get; set; }
+        public List<Priority> Priorities { get; set; }
+        public Task Task { get; set; }
+        public List<Task> Tasks { get; set; }
+        [Display(Name = "Data")]
+        public DateTime DatePick { get; set; }
+        public string Date { get; set; }
+    }
+}
