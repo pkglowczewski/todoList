856ec495f14bf928331c6c53abdd307851ff199c
Piotr Kłopotek Główczewski <pkglowczewski@gmail.com>
2022-02-19 11:42:10 +0100

    Update Home Index

--- todoList/Controllers/HomeController.cs
+++ todoList/Controllers/HomeController.cs
@@ -1,21 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
+using todoList.Data;
 using todoList.Models;
 
 namespace todoList.Controllers
-{
+{  [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _db;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var list = new List<Models.Task>();
+            var PriorityList = _db.Priorities.ToList();
+            DateTime date = DateTime.Now;
+            for (int i = 1; i <= PriorityList.Count; i++)
+            {
+                var item = _db.Tasks.Select(e => e).Where(e => e.UserId == userId && e.PriorityId == i && e.DateTime > date).OrderBy(e => e.DateTime).Take(3);
+                var itemlist = item;
+                if (itemlist != null)
+                {
+                    foreach (var itemL in itemlist)
+                    {
+                        list.Add(itemL);
+                    }
+                }
+            }
+            var vm = new TaskPriorityViewModel();
+            vm.Tasks = list;
+            vm.Priorities = PriorityList;
+            return View(vm);
         }
 
         public IActionResult Privacy()
