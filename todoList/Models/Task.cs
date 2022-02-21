74992504156a9a7dc9a7a4b6bb5e74d0ac7a65b9
Piotr Kłopotek Główczewski <pkglowczewski@gmail.com>
2022-02-21 19:55:23 +0100

    Add custom error messages to Task model

--- todoList/Models/Task.cs
+++ todoList/Models/Task.cs
@@ -8,20 +8,21 @@ namespace todoList.Models
     {
         [Key]
         public int TaskId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Uzupełnij pole Nazwa")]
         [Display(Name = "Nazwa")]
         [StringLength(100)]
         public string Name { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Uzupełnij pole Opis")]
         [Display(Name = "Opis")]
         [StringLength(300)]
         public string Description { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Uzupełnij pole Data")]
         [Display(Name = "Data")]
         public DateTime DateTime { get; set; }
         [Display(Name = "Zakończone")]
         public bool isDone { get; set; }
         [Required]
+        [Range(1,int.MaxValue, ErrorMessage ="Wybierz priorytet z listy")]
         [Display(Name = "Priorytet")]
         public int PriorityId { get; set; }
         [Display(Name = "Priorytet")]
